5347ecb6bd9e232e23ecaf7d7505c3042ba74de5
stescobedo92 <stescobedo.31@gmail.com>
2023-07-13 21:28:35 -0600

    Defining procedural macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,8 @@ use quote::quote;
 use syn::{
     parse_macro_input, AttributeArgs, Data, DataStruct, DeriveInput, Field, Lit, Meta, NestedMeta,
 };
+
+#[proc_macro_attribute]
+pub fn field(args: TokenStream, input: TokenStream) -> TokenStream {
+
+}
